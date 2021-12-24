9d58db4d29d777b27fdfe6c002e7c6fab2f0ef79
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-24 20:03:55 +0300

    Added implementations

--- grading-system/build.gradle.kts
+++ grading-system/build.gradle.kts
@@ -15,6 +15,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2:2.0.202")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
