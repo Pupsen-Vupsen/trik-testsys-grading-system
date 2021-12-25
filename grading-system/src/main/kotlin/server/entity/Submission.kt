43a4023cc56cf7af87aede64e28fba4ad228b4b1
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-25 05:27:39 +0300

    Added changing status method

--- grading-system/src/main/kotlin/server/entity/Submission.kt
+++ grading-system/src/main/kotlin/server/entity/Submission.kt
@@ -9,6 +9,12 @@ class Submission(val filePath: String? = null) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0
+        private set
 
     var status: String = "running"
+        private set
+
+    fun changeStatus(newStatus: String) {
+        status = newStatus
+    }
 }
