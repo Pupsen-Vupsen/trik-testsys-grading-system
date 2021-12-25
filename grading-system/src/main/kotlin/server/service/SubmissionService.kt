0175b8f4f8ed4263bd521ce4ed240a6a29e19aed
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-25 04:16:39 +0300

    Added submissions service

---
+++ grading-system/src/main/kotlin/server/service/SubmissionService.kt
@@ -0,0 +1,23 @@
+package server.service
+
+import server.entity.Submission
+import server.repository.SubmissionRepository
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class SubmissionService {
+
+    @Autowired
+    lateinit var submissionRepository: SubmissionRepository
+
+    fun getSubmission(id: Long) = submissionRepository.findSubmissionById(id)
+
+    fun getAllSubmissions(): Iterable<Submission> = submissionRepository.findAll()
+
+    fun saveSubmission(submission: Submission): Long? {
+        submissionRepository.save(submission)
+        return submission.id
+    }
+}
