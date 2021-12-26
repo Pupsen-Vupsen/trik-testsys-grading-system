6f1f88112707721555f5e76e4ca358bfdf96c14f
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-26 15:31:38 +0300

    Added methods to get and post submissions and validating them not to post same running submissions

--- grading-system/src/main/kotlin/server/controller/SubmissionController.kt
+++ grading-system/src/main/kotlin/server/controller/SubmissionController.kt
@@ -15,20 +15,42 @@ class SubmissionController {
     private lateinit var submissionService: SubmissionService
 
     @GetMapping("grading-system/submissions")
-    fun getAllSubmission(): ResponseEntity<Iterable<Submission>> {
+    fun getAllSubmission(): ResponseEntity<Any> {
         val submissions = submissionService.getAllSubmissions()
-        return ResponseEntity.status(HttpStatus.OK).body(submissions)
+
+        if (submissions.isEmpty())
+            return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body("There is no submissions.")
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(submissions)
     }
 
-    @GetMapping("grading-system/submission")
-    fun getSubmission(id: Long): ResponseEntity<String> {
+    @GetMapping("grading-system/submissions/submission")
+    fun getSubmission(@RequestParam id: Long): ResponseEntity<String> {
         val submission = submissionService.getSubmission(id)
-        return ResponseEntity.status(HttpStatus.OK).body(submission?.status)
+            ?: return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body("Submission with id $id does not exist.")
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(submission.status)
     }
 
-    @PutMapping("grading-system/submission")
-    fun postSubmission(@RequestParam filePath: String): ResponseEntity<Long> {
+    @PostMapping("grading-system/submissions/submission")
+    fun postSubmission(@RequestParam filePath: String): ResponseEntity<Any> {
+        submissionService.getSameRunningSubmission(filePath)?.let {
+            return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("You have already running submission on this task.")
+        }
+
         val submissionId = submissionService.saveSubmission(Submission(filePath))
-        return ResponseEntity.status(HttpStatus.CREATED).body(submissionId)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(submissionId)
     }
 }
