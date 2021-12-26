9b90a4ead25f57361c1f2ccffbcfe8f12e6c07c4
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-26 19:12:31 +0300

    Added changing status and bigger id generating

--- grading-system/src/main/kotlin/server/entity/Submission.kt
+++ grading-system/src/main/kotlin/server/entity/Submission.kt
@@ -1,20 +1,35 @@
 package server.entity
 
+import org.springframework.scheduling.annotation.Async
+import java.util.concurrent.TimeUnit
 import javax.persistence.*
 
 @Entity
-@Table(name = "t_submission")
+@Table(name = "SUBMISSIONS")
 class Submission(val filePath: String? = null) {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0
-        private set
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private val tableId: Long = 0
+
+    val id: Int = this.hashCode()
 
     var status: String = "running"
         private set
 
-    fun changeStatus(newStatus: String) {
-        status = newStatus
+    fun changeStatus() {
+        val n = (0..1).random()
+        TimeUnit.SECONDS.sleep(15)
+
+        if (n == 0) accept()
+        else deny()
+    }
+
+    private fun accept() {
+        status = "ok"
+    }
+
+    private fun deny() {
+        status = "failed"
     }
 }
