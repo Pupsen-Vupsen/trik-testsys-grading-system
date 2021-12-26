8bf509a1257c7b0a838e30a585ddb1905385cbd4
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-27 00:30:16 +0300

    Added generating id from 1000000

--- grading-system/src/main/kotlin/server/entity/Submission.kt
+++ grading-system/src/main/kotlin/server/entity/Submission.kt
@@ -1,6 +1,5 @@
 package server.entity
 
-import org.springframework.scheduling.annotation.Async
 import java.util.concurrent.TimeUnit
 import javax.persistence.*
 
@@ -9,17 +8,16 @@ import javax.persistence.*
 class Submission(val filePath: String? = null) {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private val tableId: Long = 0
-
-    val id: Int = this.hashCode()
+    @SequenceGenerator(name = "seq", initialValue = 1000000, allocationSize = 10000)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="seq")
+    val id: Long = 0
 
     var status: String = "running"
         private set
 
     fun changeStatus() {
         val n = (0..1).random()
-        TimeUnit.SECONDS.sleep(15)
+        TimeUnit.SECONDS.sleep(60)
 
         if (n == 0) accept()
         else deny()
