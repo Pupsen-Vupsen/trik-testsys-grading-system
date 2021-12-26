e5cca14e1bd719588b485c658e80ee993820f7b7
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-26 19:14:27 +0300

    Changed id type

--- grading-system/src/main/kotlin/server/repository/SubmissionRepository.kt
+++ grading-system/src/main/kotlin/server/repository/SubmissionRepository.kt
@@ -3,10 +3,12 @@ package server.repository
 import server.entity.Submission
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SubmissionRepository: CrudRepository<Submission, String> {
 
-    fun findSubmissionById(id: Long): Submission?
+    fun findSubmissionById(id: Int): Submission?
 
     fun findSubmissionByFilePathAndStatus(filePath: String, status: String): Submission?
 }
