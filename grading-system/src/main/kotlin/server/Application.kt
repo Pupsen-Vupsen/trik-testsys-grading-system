e930bd0166b03cb388ea7fcc218185071d09765f
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-28 05:02:25 +0300

    Updated file configuration

--- grading-system/src/main/kotlin/server/Application.kt
+++ grading-system/src/main/kotlin/server/Application.kt
@@ -1,11 +1,30 @@
 package server
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.MultipartConfigFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.util.unit.DataSize
+import org.springframework.util.unit.DataUnit
+import javax.servlet.MultipartConfigElement
+
 
 @SpringBootApplication
-class Application
+@Configuration
+class Application {
+    @Bean
+    fun multipartConfigElement(): MultipartConfigElement {
+        val factory = MultipartConfigFactory()
+        factory.setMaxFileSize(DataSize.of(4, DataUnit.MEGABYTES))
+        factory.setMaxRequestSize(DataSize.of(4, DataUnit.MEGABYTES))
+        return factory.createMultipartConfig()
+    }
 
-fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
 }
