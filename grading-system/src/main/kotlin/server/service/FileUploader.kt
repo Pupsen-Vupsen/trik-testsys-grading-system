2ded47f9aab1230e5b7b999ba058f0447209e1f8
ShishaK1N9 <st077017@student.spbu.ru>
2021-12-28 06:34:18 +0300

    Cosmetic fixes

--- grading-system/src/main/kotlin/server/service/FileUploader.kt
+++ grading-system/src/main/kotlin/server/service/FileUploader.kt
@@ -1,6 +1,7 @@
 package server.service
 
 import org.springframework.web.multipart.MultipartFile
+
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -8,7 +9,7 @@ import java.io.FileOutputStream
 class FileUploader(private val file: MultipartFile, private val fileName: String) {
 
     fun upload(directory: String): Boolean {
-        if(file.isEmpty) return false
+        if (file.isEmpty) return false
 
         val bytes = file.bytes
         val stream = BufferedOutputStream(FileOutputStream(File(fileName)))
