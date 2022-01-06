eaba6de9afd3741564a2919928b3ada1430cf91e
ShishaK1N9 <st077017@student.spbu.ru>
2022-01-06 06:28:12 +0300

    Updated FileUploader

--- grading-system/src/main/kotlin/server/service/FileUploader.kt
+++ grading-system/src/main/kotlin/server/service/FileUploader.kt
@@ -6,13 +6,15 @@ import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
 
-class FileUploader(private val file: MultipartFile, private val fileName: String) {
+class FileUploader(private val file: MultipartFile, private val taskNumber: String) {
 
-    fun upload(directory: String): Boolean {
+    fun upload(): Boolean {
         if (file.isEmpty) return false
 
         val bytes = file.bytes
-        val stream = BufferedOutputStream(FileOutputStream(File(fileName)))
+        val uploadedFile = File("C:\\Projects\\Git\\trik-testsys\\tasks\\task" + taskNumber + "\\" + file.originalFilename)
+        val stream = BufferedOutputStream(FileOutputStream(uploadedFile))
+
         stream.write(bytes)
         stream.close()
 
