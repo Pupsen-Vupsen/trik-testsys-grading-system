583709aaa0c1cd788001bbb61dd3675eb560a04c
ShishaK1N9 <st077017@student.spbu.ru>
2022-01-07 05:55:47 +0300

    Testing parsing json

--- grading-system/src/main/kotlin/server/entity/Submission.kt
+++ grading-system/src/main/kotlin/server/entity/Submission.kt
@@ -1,7 +1,9 @@
 package server.entity
 
+import com.beust.klaxon.Klaxon
 import server.enum.Status
 
+import java.io.File
 import javax.persistence.*
 
 @Entity
@@ -9,21 +11,43 @@ import javax.persistence.*
 class Submission(val filePath: String = "") {
 
     @Id
-    @SequenceGenerator(name = "seq", initialValue = 1000000, allocationSize = 10000)
+    @SequenceGenerator(name = "seq", initialValue = 1_000_000, allocationSize = 10_000)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
     val id: Long = 0
 
     var status = Status.RUNNING
         private set
 
+    var message = ""
+        private set
+
     fun test() {
-        Runtime.getRuntime().exec("powershell.exe -File ./test_submission.ps1")
+        class TestingResults(val level: String, val message: String)
 
-        /*val n = (0..1).random()
-            TimeUnit.SECONDS.sleep(60)
+        Runtime.getRuntime().exec("C:\\TRIKStudio\\2D-model.exe -b -r $filePath.log  $filePath")
+        Thread.sleep(10_000)
+        /* val logFile = File("$filePath.log").inputStream()
+        val strings = logFile.bufferedReader().use { it.readText() }
 
-            if (n == 0) accept()
-            else deny()*/
+        if(strings[26] == 'e') deny()
+        else accept()*/
+        val logFile = File("$filePath.log")
+        /*while(!logFile.exists()) {
+            logFile = File("$filePath.log")
+        }*/
+
+        val log = Klaxon().parseArray<TestingResults>(logFile)!!
+        /*while(log == null) {
+            log = Klaxon().parse<TestingResults>(File("$filePath.log"))
+        }*/
+
+        if(log[0].level == "error") deny()
+        else accept()
+
+        message = log[0].message
+
+        println(logFile.absolutePath)
+        //Runtime.getRuntime().exec("del ${logFile.absolutePath}")
     }
 
     private fun accept() {
