f379d699c550e46232c623e07d7833c4c6fa8ed5
5h15h4k1n9 <st077017@student.spbu.ru>
2022-03-15 15:42:18 +0300

    Added logging to SubmissionController

--- src/main/kotlin/server/controller/SubmissionController.kt
+++ src/main/kotlin/server/controller/SubmissionController.kt
@@ -13,20 +13,30 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.FileInputStream
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @RestController
 class SubmissionController {
+
+    val logger: Logger = LoggerFactory.getLogger(SubmissionController::class.java)
 
     @Autowired
     private lateinit var submissionService: SubmissionService
 
     @GetMapping("grading-system/submissions")
     fun getAllSubmissions(): ResponseEntity<Any> {
-        val submissions = submissionService.getAllSubmissionsOrNull()
-            ?: return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(Requests.NO_SUBMISSIONS)
+        logger.info("Client requested all submissions.")
 
+        val submissions = submissionService.getAllSubmissionsOrNull()
+            ?: run {
+                logger.warn("There are no submissions.")
+                return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(Requests.NO_SUBMISSIONS)
+            }
+
+        logger.info("Returned all submissions.")
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(submissions)
@@ -34,11 +44,17 @@ class SubmissionController {
 
     @GetMapping("grading-system/submissions/submission/status")
     fun getSubmissionStatus(@RequestParam id: Long): ResponseEntity<String> {
-        val submission = submissionService.getSubmissionOrNull(id)
-            ?: return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(Requests.SUBMISSION_NOT_FOUND)
+        logger.info("Client requested submission $id status.")
 
+        val submission = submissionService.getSubmissionOrNull(id)
+            ?: run {
+                logger.warn("There is no submission with id $id")
+                return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(Requests.SUBMISSION_NOT_FOUND)
+            }
+
+        logger.info("Returned submission $id status.")
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(submission.status)
@@ -46,13 +62,23 @@ class SubmissionController {
 
     @GetMapping("grading-system/submissions/submission/download")
     fun getSubmissionFile(@RequestParam id: Long): ResponseEntity<Any> {
+        logger.info("Client requested submission $id file.")
         val submission = submissionService.getSubmissionOrNull(id)
-            ?: return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(Requests.SUBMISSION_NOT_FOUND)
+            ?: run {
+                logger.warn("There is no submission with id $id")
+                return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(Requests.SUBMISSION_NOT_FOUND)
+            }
 
-        val stream = InputStreamResource(FileInputStream(File(submission.filePath)))
+        val stream = try {
+            InputStreamResource(FileInputStream(File(submission.filePath)))
+        } catch (e: Exception) {
+            logger.error("Caught exception: $e!")
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.toString())
+        }
 
+        logger.info("Returned submission $id file.")
         return ResponseEntity.status(HttpStatus.OK).body(stream)
     }
 
@@ -63,9 +89,13 @@ class SubmissionController {
         @RequestParam("task_name") taskName: String,
         @RequestParam file: MultipartFile
     ): ResponseEntity<Any> {
+        logger.info("Got file!")
+
         if (submissionId == ID.DEFAULT_ID)
             submissionId = submissionService.getLastSubmissionIdOrNull() ?: ID.FIRST_ID
         submissionId++
+
+        logger.info("Set $submissionId to new file.")
 
         val fileName = "$submissionId.qrs"
         val fileUploader = FileUploader(file, "$taskName/$fileName")
@@ -75,14 +105,20 @@ class SubmissionController {
                 submissionService.saveSubmission(Submission(submissionId, taskName, fileName))
                 submissionService.testSubmission(submissionId)
 
+                logger.info("Saved submission $submissionId.")
                 ResponseEntity
                     .status(HttpStatus.OK)
                     .body(submissionId)
-            } else
+            } else {
+                logger.warn("Uploading file is empty.")
+
                 ResponseEntity
                     .status(HttpStatus.UNPROCESSABLE_ENTITY)
                     .body(Requests.EMPTY_FILE)
+            }
         } catch (e: Exception) {
+            logger.error("Caught exception: $e!")
+
             ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(e.message)
