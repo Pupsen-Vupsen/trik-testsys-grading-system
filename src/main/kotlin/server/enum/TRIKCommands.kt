a21eec8e09448fbd43535cc4d149c2c394dde03a
5h15h4k1n9 <st077017@studetn.spbu.ru>
2022-10-18 12:17:26 +0300

    Changed 2d-model parameters

--- src/main/kotlin/server/enum/TRIKCommands.kt
+++ src/main/kotlin/server/enum/TRIKCommands.kt
@@ -2,6 +2,6 @@ package server.enum
 
 enum class TRIKCommands(val command: String) {
 
-    TWO_D_MODEL("./TRIKStudio/bin/2D-model -platform offscreen -s 10 -b -r"),
+    TWO_D_MODEL("./TRIKStudio/bin/2D-model -platform offscreen --close -r"),
     PATCHER("./TRIKStudio/bin/patcher -f")
 }
