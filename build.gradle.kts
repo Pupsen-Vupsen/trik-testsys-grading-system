425ca18a28cbecc9aa6cb6edda8cc976aa6d9a67
5h15h4k1n9 <st077017@studetn.spbu.ru>
2022-11-28 08:10:22 +0300

    Added jacoco config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    jacoco
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
@@ -30,6 +31,8 @@ dependencies {
     implementation("com.beust:klaxon:5.6")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation(platform("org.junit:junit-bom:5.9.0"))
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -38,6 +41,41 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     }
 }
 
-tasks.withType<Test> {
+val jacocoExclude = listOf(
+    "**/configuration/**",
+    "**/entity/**",
+    "**/enum/**",
+    "**/repository/**",
+    "**/service/**",
+    "**/*Application*"
+)
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
     useJUnitPlatform()
+    maxHeapSize = "2G"
+    extensions.configure<JacocoTaskExtension> {
+        excludes = jacocoExclude
+    }
+    // Uncomment to run concurrent tests on your own PC
+    /*jvmArgs(
+        "-Xmx4096m",
+        "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
+        "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED"
+    )*/
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        csv.required.set(true)
+        csv.outputLocation.set(file("${buildDir}/jacoco/report.csv"))
+        html.outputLocation.set(file("${buildDir}/reports/jacoco"))
+    }
+    classDirectories.setFrom(classDirectories.files.map {
+        fileTree(it).matching {
+            exclude(jacocoExclude)
+        }
+    })
 }
