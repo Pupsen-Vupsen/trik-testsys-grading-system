56a8e9d96b45648e60eea420009e298e7ac58828
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-03-09 00:26:19 +0300

    Refactored statuses

--- src/main/kotlin/trik/testsys/gradingsystem/enums/Status.kt
+++ src/main/kotlin/trik/testsys/gradingsystem/enums/Status.kt
@@ -1,12 +1,17 @@
 package trik.testsys.gradingsystem.enums
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonValue
 
 
+@JsonFormat(shape = JsonFormat.Shape.NUMBER)
 enum class Status(val code: Int) {
-
     QUEUED(100),
     RUNNING(102),
     ACCEPTED(202),
     FAILED(406),
     ERROR(500);
+
+    @JsonValue
+    fun toValue() = code
 }
